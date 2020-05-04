b92207b4b11b16c5e9f376f97ec1e56235a1f9f6
Dustin Oprea <dustin@randomingenuity.com>
2020-05-04 04:04:31 -0400

    media_parser.go: Return encountered segments even when failing
    Allows the caller to see whether they got what they needed and
    potentially avoid failure.
    
    - Added missing comments.

--- media_parser.go
+++ media_parser.go
@@ -10,13 +10,18 @@ import (
     "github.com/dsoprea/go-utility/image"
 )
 
+// JpegMediaParser is a `riimage.MediaParser` that knows how to parse JPEG
+// images.
 type JpegMediaParser struct {
 }
 
+// NewJpegMediaParser returns a new JpegMediaParser.
 func NewJpegMediaParser() *JpegMediaParser {
     return new(JpegMediaParser)
 }
 
+// Parse parses a JPEG uses an `io.ReadSeeker`. Even if it fails, it will return
+// the list of segments encountered prior to the failure.
 func (jmp *JpegMediaParser) Parse(rs io.ReadSeeker, size int) (ec riimage.MediaContext, err error) {
     defer func() {
         if state := recover(); state != nil {
@@ -37,11 +42,17 @@ func (jmp *JpegMediaParser) Parse(rs io.ReadSeeker, size int) (ec riimage.MediaC
     for s.Scan() != false {
     }
 
+    // Always return the segments that were parsed, at least until there was an
+    // error.
+    ec = js.Segments()
+
     log.PanicIf(s.Err())
 
-    return js.Segments(), nil
+    return ec, nil
 }
 
+// ParseFile parses a JPEG file. Even if it fails, it will return the list of
+// segments encountered prior to the failure.
 func (jmp *JpegMediaParser) ParseFile(filepath string) (ec riimage.MediaContext, err error) {
     defer func() {
         if state := recover(); state != nil {
@@ -58,11 +69,18 @@ func (jmp *JpegMediaParser) ParseFile(filepath string) (ec riimage.MediaContext,
     size := stat.Size()
 
     sl, err := jmp.Parse(f, int(size))
+
+    // Always return the segments that were parsed, at least until there was an
+    // error.
+    ec = sl
+
     log.PanicIf(err)
 
-    return sl, nil
+    return ec, nil
 }
 
+// ParseBytes parses a JPEG byte-slice. Even if it fails, it will return the
+// list of segments encountered prior to the failure.
 func (jmp *JpegMediaParser) ParseBytes(data []byte) (ec riimage.MediaContext, err error) {
     defer func() {
         if state := recover(); state != nil {
@@ -73,11 +91,17 @@ func (jmp *JpegMediaParser) ParseBytes(data []byte) (ec riimage.MediaContext, er
     br := bytes.NewReader(data)
 
     sl, err := jmp.Parse(br, len(data))
+
+    // Always return the segments that were parsed, at least until there was an
+    // error.
+    ec = sl
+
     log.PanicIf(err)
 
-    return sl, nil
+    return ec, nil
 }
 
+// LooksLikeFormat indicates whether the data looks like a JPEG image.
 func (jmp *JpegMediaParser) LooksLikeFormat(data []byte) bool {
     if len(data) < 4 {
         return false
