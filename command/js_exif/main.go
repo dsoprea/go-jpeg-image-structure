d28cfe053b347705eeca0226559588c76d4e5213
Dustin Oprea <dustin@randomingenuity.com>
2020-05-16 17:36:09 -0400

    js_exif/main.go: Format values

--- command/js_exif/main.go
+++ command/js_exif/main.go
@@ -143,7 +143,7 @@ func main() {
 					continue
 				}
 
-				fmt.Printf("%2d: IFD-PATH=[%s] ID=(0x%02x) NAME=[%s] TYPE=(%d):[%s] VALUE=[%v]", i, tag.IfdPath, tag.TagId, tag.TagName, tag.TagTypeId, tag.TagTypeName, tag.Value)
+				fmt.Printf("%2d: IFD-PATH=[%s] ID=(0x%02x) NAME=[%s] TYPE=(%d):[%s] VALUE=[%v]", i, tag.IfdPath, tag.TagId, tag.TagName, tag.TagTypeId, tag.TagTypeName, tag.FormattedFirst)
 
 				if tag.ChildIfdPath != "" {
 					fmt.Printf(" CHILD-IFD-PATH=[%s]", tag.ChildIfdPath)
